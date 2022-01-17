8182ff7de259e13c6ff05c2ea89ba774f50dacbd
Johnny Carpio <johnnycarpio04@gmail.com>
2022-01-16 22:55:08 -0800

    disabled broser default behaviour for submitting tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 // Creating a variable for certain HTML IDs
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 // Creating a variable for a function that we will call repeatedly (this has to be before the event listener or it will result in an undefined error)
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+// Default browser behaviour is to refresh the page any time something is submitted, this line makes sure we override that behaviour
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-// Every time the buttonEl is clicked, createTaskHandler function will run
-buttonEl.addEventListener("click", createTaskHandler);
+// Every time the anything on #task-form is submitted, createTaskHandler function will run
+formEl.addEventListener("submit", createTaskHandler);
