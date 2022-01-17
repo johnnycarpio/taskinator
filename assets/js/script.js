aa13079dafa57d1d6a21e112bd46f410a7e4fda4
Johnny Carpio <johnnycarpio04@gmail.com>
2022-01-16 23:21:19 -0800

    user input for submit and tasktype now gets pushed into the tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,31 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Creating a variable for a function that we will call repeatedly (this has to be before the event listener or it will result in an undefined error)
-var createTaskHandler = function(event) {
-// Default browser behaviour is to refresh the page any time something is submitted, this line makes sure we override that behaviour
-    event.preventDefault();
+var createTaskHandler = function (event) {
 
+    event.preventDefault();
+    // Creating a variable for VALUE that is provided in the INPUT section under the (name="task-name")
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput)
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+
+    console.dir(listItemEl);
 }
 
 // Every time the anything on #task-form is submitted, createTaskHandler function will run
